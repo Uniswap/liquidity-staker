bae71f11721dda5a3e19f37d274a94df381f7912
Noah Zinsmeister <noahwz@gmail.com>
2020-09-02 12:47:18 -0400

    add preliminary tests

--- test/StakingRewards.spec.ts
+++ test/StakingRewards.spec.ts
@@ -1,8 +1,9 @@
-import chai from 'chai'
-import { Contract } from 'ethers'
+import chai, { expect } from 'chai'
+import { Contract, BigNumber } from 'ethers'
 import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
 
 import { stakingRewardsFixture } from './fixtures'
+import { REWARDS_DURATION, expandTo18Decimals, mineBlock } from './utils'
 
 chai.use(solidity)
 
@@ -14,7 +15,7 @@ describe('StakingRewards', () => {
       gasLimit: 9999999,
     },
   })
-  const [wallet] = provider.getWallets()
+  const [wallet, staker] = provider.getWallets()
   const loadFixture = createFixtureLoader([wallet], provider)
 
   let stakingRewards: Contract
@@ -27,7 +28,41 @@ describe('StakingRewards', () => {
     stakingToken = fixture.stakingToken
   })
 
-  it('test', async () => {
-    console.log('great success')
+  it('rewardsDuration', async () => {
+    const rewardsDuration = await stakingRewards.rewardsDuration()
+    expect(rewardsDuration).to.be.eq(REWARDS_DURATION)
+  })
+
+  it('notifyRewardAmount', async () => {
+    // send reward to the contract
+    const reward = expandTo18Decimals(100)
+    await rewardsToken.transfer(stakingRewards.address, reward)
+    // must be called by rewardsDistribution
+    await stakingRewards.notifyRewardAmount(reward)
+
+    const rewardsStartTime: BigNumber = await stakingRewards.lastUpdateTime()
+    const periodFinish: BigNumber = await stakingRewards.periodFinish()
+    expect(periodFinish).to.be.eq(rewardsStartTime.add(REWARDS_DURATION))
+
+    // fast-forward ~halfway through the reward window
+    await mineBlock(provider, rewardsStartTime.add(Math.floor(REWARDS_DURATION / 2)).toNumber())
+
+    // stake with staker
+    const stake = expandTo18Decimals(2)
+    await stakingToken.transfer(staker.address, stake)
+    await stakingToken.connect(staker).approve(stakingRewards.address, stake)
+    await stakingRewards.connect(staker).stake(stake)
+    const stakeStartTime: BigNumber = await stakingRewards.lastUpdateTime()
+
+    // fast-forward past the reward window
+    await mineBlock(provider, periodFinish.add(1).toNumber())
+
+    // unstake
+    await stakingRewards.connect(staker).exit()
+    const stakeEndTime: BigNumber = await stakingRewards.lastUpdateTime()
+    expect(stakeEndTime).to.be.eq(periodFinish)
+
+    const rewardAmount = await rewardsToken.balanceOf(staker.address)
+    expect(reward.div(REWARDS_DURATION).mul(periodFinish.sub(stakeStartTime))).to.be.eq(rewardAmount)
   })
 })
--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,11 @@
-import { BigNumber } from 'ethers'
+import { BigNumber, providers } from 'ethers'
+
+export const REWARDS_DURATION = 60 * 60 * 24 * 7
 
 export function expandTo18Decimals(n: number): BigNumber {
   return BigNumber.from(n).mul(BigNumber.from(10).pow(18))
+}
+
+export async function mineBlock(provider: providers.Web3Provider, timestamp: number): Promise<void> {
+  return provider.send('evm_mine', [timestamp])
 }
