6148b96dbec78f028d184b0c060bd2f068229da3
Noah Zinsmeister <noahwz@gmail.com>
2020-09-06 20:02:59 -0400

    7 -> 60 days

--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,6 @@
 import { BigNumber, providers } from 'ethers'
 
-export const REWARDS_DURATION = 60 * 60 * 24 * 7
+export const REWARDS_DURATION = 60 * 60 * 24 * 60
 
 export function expandTo18Decimals(n: number): BigNumber {
   return BigNumber.from(n).mul(BigNumber.from(10).pow(18))
